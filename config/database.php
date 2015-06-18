1694e54d07d7c3d7ca936fce4a2b38a56f0aaacf
T4GAD4 <capi.aurelien@gmail.com>
2015-06-18 22:24:49 +0200

    Connexion Database!

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$database = "projetphp_sql1";
+$database = "projetphp_sql";
 
 $mysqli = new mysqli($server, $username, $password, $database);
 
