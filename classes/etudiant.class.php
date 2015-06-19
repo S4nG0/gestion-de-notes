30f7dda1c6a1141b492859a79facc9a9b0968725
T4GAD4 <capi.aurelien@gmail.com>
2015-06-19 13:32:36 +0200

    Modification Classes

--- classes/etudiant.class.php
+++ classes/etudiant.class.php
@@ -1,11 +1,12 @@
 <?php
 
+
 class Etudiant extends Personne{
 
     public function Etudiant(){
-        
+        parent::Personne();
         $db = Database::getInstance();
         $req = $db->query("SELECT * FROM note WHERE idPersonne = '". $_SESSION['personne']->idPersonne ."'");
-        var_dump($req);
+        
     }
 }
