04a6b86b261415b536b716012ad8819b1db83eba
S4nG0 <julien.vdm@gmail.com>
2015-06-19 13:48:15 +0200

    Page error cree + changement index.php

---
+++ error.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+<div class="container error">
+    <div class="row">
+        <h1>Erreur</h1>
+        <p>Votre status n'est pas reconnu.</p>
+    </div>
+    <div class="row">
+        <a href="index.php" class="bottom-link">Retour à la page de connexion.</a>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
