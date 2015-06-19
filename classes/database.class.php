e761c089e4e40521e1e51bdcd14c4e59aa9e6b86
S4nG0 <julien.vdm@gmail.com>
2015-06-19 16:53:55 +0200

    corrections sur etudiants

--- classes/database.class.php
+++ classes/database.class.php
@@ -4,14 +4,16 @@ class Database
     private static $instance = NULL;
 
     public static function getInstance() {
-        
+
         $server = "localhost";
         $username = "root";
         $password = "";
         $database = "phpspe_gestionDeNotes";
+        $charset = "utf8";
 
         if (!self::$instance) {
             self::$instance = new mysqli($server, $username, $password, $database);
+            self::$instance->set_charset($charset);
         }
         return self::$instance;
     }
