d629eb163819f0d028600603d3e6ee242dc43479
S4nG0 <julien.vdm@gmail.com>
2015-06-19 16:36:49 +0200

    modifications accueil enseignant + index

--- index.php
+++ index.php
@@ -80,7 +80,6 @@ if( (isset($_POST['identifiant']) && !empty($_POST['identifiant'])) && (isset($_
                     </div>
                 </div>
             </form>
-
         </div>
     </div>
 <?php
